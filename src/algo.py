e14384f30b0356bf93cf883cbe32b03534ccf7f7
AlbertChoe <albert.choe73@gmail.com>
2024-02-10 21:42:41 +0700

    milestone 1

---
+++ src/algo.py
@@ -0,0 +1,168 @@
+import random
+
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            buffer_size = int(file.readline().strip())
+
+            matrix_width, matrix_height = map(
+                int, file.readline().strip().split())
+
+            matrix = [file.readline().strip() for _ in range(matrix_height)]
+
+            number_of_sequences = int(file.readline().strip())
+
+            sequences = []
+            for _ in range(number_of_sequences):
+                sequence = file.readline().strip()
+                reward = file.readline().strip()
+                sequences.append((sequence, reward))
+
+        return {
+            'buffer_size': buffer_size,
+            'matrix_dimensions': (matrix_width, matrix_height),
+            'matrix': matrix,
+            'sequences': sequences
+        }
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None
+
+
+def find_matching(buffer, sequences):
+    total_reward = 0
+    buffer_str = ' '.join(buffer)
+    for sequence, reward in sequences:
+        sequence_str = ' '.join(sequence)
+        if sequence_str in buffer_str:
+            total_reward += int(reward)
+    return total_reward
+
+
+def trim_path(matrix, sequences, best_path):
+    max_reward = find_matching([matrix[pos[0]][pos[1]]
+                               for pos in best_path], sequences)
+    for i in range(len(best_path), 0, -1):
+        temp_path = best_path[:i]
+        buffer = [matrix[pos[0]][pos[1]] for pos in temp_path]
+        temp_reward = find_matching(buffer, sequences)
+
+        if temp_reward < max_reward:
+            return best_path[:i+1]
+
+    return best_path
+
+
+def solve_algo(matrix, sequences, buffer_size, path=[], direction='H', best_reward=0, best_path=[], best_trimmed_path=[]):
+    if len(path) == buffer_size:
+        buffer = [matrix[pos[0]][pos[1]] for pos in path]
+        reward = find_matching(buffer, sequences)
+        if reward > best_reward:
+            best_reward = reward
+            best_path = path[:]
+            best_trimmed_path = trim_path(matrix, sequences, best_path)
+        return best_reward, best_path, best_trimmed_path
+
+    if not path:
+        for i in range(len(matrix[0])):
+            temp_reward, temp_path, temp_trimmed_path = solve_algo(
+                matrix, sequences, buffer_size, [(0, i)], 'V', best_reward, best_path, best_trimmed_path)
+            if temp_reward > best_reward:
+                best_reward, best_path, best_trimmed_path = temp_reward, temp_path, temp_trimmed_path
+    else:
+        last_pos = path[-1]
+        next_positions = []
+        if direction == 'H':
+            for i in range(len(matrix[0])):
+                if i != last_pos[1]:
+                    next_positions.append((last_pos[0], i))
+        else:
+            for i in range(len(matrix)):
+                if i != last_pos[0]:
+                    next_positions.append((i, last_pos[1]))
+
+        for next_pos in next_positions:
+            if next_pos not in path:
+                new_path = path + [next_pos]
+                new_direction = 'V' if direction == 'H' else 'H'
+                temp_reward, temp_path, temp_trimmed_path = solve_algo(
+                    matrix, sequences, buffer_size, new_path, new_direction, best_reward, best_path, best_trimmed_path)
+                if temp_reward > best_reward:
+                    best_reward, best_path, best_trimmed_path = temp_reward, temp_path, temp_trimmed_path
+
+    return best_reward, best_path, best_trimmed_path
+
+
+def generate_data():
+    while True:
+        try:
+            num_unique_tokens = int(input("Number of unique tokens: "))
+            tokens = input("Tokens (space-separated): ").split()
+            buffer_size = int(input("Buffer size: "))
+            matrix_size = input("Matrix size (width height): ").split()
+            matrix_width, matrix_height = int(
+                matrix_size[0]), int(matrix_size[1])
+            num_sequences = int(input("Number of sequences: "))
+            max_sequence_size = int(input("Maximum sequence size: "))
+
+            if len(tokens) < num_unique_tokens or len(tokens) > num_unique_tokens:
+                raise ValueError(
+                    "Error: More unique tokens requested than provided.")
+
+            break
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please try again.")
+
+    # Generate Matrix
+    matrix = [[random.choice(tokens) for _ in range(matrix_width)]
+              for _ in range(matrix_height)]
+
+    # Generate sequences
+    sequences = []
+    for _ in range(num_sequences):
+        # Memastikan panjang sequence tidak melebihi batas
+        seq_length = random.randint(2, min(max_sequence_size, len(tokens)))
+        sequence = random.sample(tokens, k=seq_length)
+        # reward antara 5 sampai 25 dan dikali dengan 5
+        reward = random.randint(5, 25) * 5
+        sequences.append((' '.join(sequence), str(reward)))
+
+    return buffer_size, matrix, sequences
+
+
+def buffer_size_validator(buffer_size, matrix):
+    if buffer_size < 2:
+        print("There are no solution for your buffer size.")
+        return 0
+    elif buffer_size > ((len(matrix)) * (len(matrix[0]))):
+        return ((len(matrix)) * (len(matrix[0])))
+    else:
+        return buffer_size
+
+
+def sequence_validator(sequences, buffer_size):
+    adjusted_sequences = []
+    for sequence, reward in sequences:
+        if len(sequence) > buffer_size:
+            adjusted_sequence = sequence[:buffer_size]
+        else:
+            adjusted_sequence = sequence
+        adjusted_sequences.append((adjusted_sequence, reward))
+    return adjusted_sequences
+
+
+def save_to_file(best_reward=0, best_path=[], best_trimmed_path=[], time_taken=0, file_name='result.txt'):
+    # Convert each position tuple into a string
+    path_tokens_str = [
+        '' + ''.join(map(str, pos)) + '' for pos in best_path]
+    trimmed_path_tokens_str = [
+        '(' + ', '.join(map(str, pos)) + ')' for pos in best_trimmed_path]
+
+    with open(file_name, 'w') as file:
+        file.write(f"Best Reward: {best_reward}\n")
+        file.write("Best Path Tokens: " + ' '.join(path_tokens_str) + "\n")
+        file.write("Best Trimmed Path Tokens: " +
+                   ' '.join(trimmed_path_tokens_str) + "\n")
+        file.write(f"Time taken: {time_taken:.2f} ms\n")
+    print(f"Results saved to {file_name}.")
---
+++ src/main.py
@@ -0,0 +1,77 @@
+import os
+from algo import read_file, solve_algo, generate_data, buffer_size_validator, sequence_validator, save_to_file
+import time
+
+
+def main():
+    data_source = input(
+        "Use file input or terminal input? (file/terminal): ").lower()
+    if data_source == 'file':
+        file_path = input("txt file name : ")
+        data = read_file(file_path)
+        if data == None:
+            return
+        buffer_size = data['buffer_size']
+        processed_matrix = [row.split() for row in data['matrix']]
+        processed_sequences = [(sequence[0].split(), sequence[1])
+                               for sequence in data['sequences']]
+    elif data_source == 'terminal':
+        buffer_size, matrix, sequences = generate_data()
+        processed_matrix = matrix
+        processed_sequences = [(sequence[0].split(), sequence[1])
+                               for sequence in sequences]
+
+    else:
+        print("Invalid input. Exiting.")
+        return
+
+    # Process the matrix and sequences
+    # print("Buffer Size:", buffer_size)
+    buffer_size = buffer_size_validator(buffer_size, processed_matrix)
+    if buffer_size == 0:
+        save_output = input(
+            "\nDo you want to save the results to a file? (yes/no): ")
+        if save_output.lower() == 'yes':
+            save_to_file()
+        return
+    processed_sequences = sequence_validator(
+        processed_sequences, buffer_size)
+    # print("Processed Matrix:")
+    # for row in processed_matrix:
+    #     print(' '.join(row))
+
+    # print("\nProcessed Sequences:")
+    # for sequence, reward in processed_sequences:
+    #     print(f"{' '.join(sequence)} with reward {reward}")
+    start_time = time.time()
+    best_reward, best_path, best_trimmed_path = solve_algo(
+        processed_matrix, processed_sequences, buffer_size)
+
+    end_time = time.time()
+    time_taken = (end_time - start_time) * 1000
+
+    path_tokens = [processed_matrix[pos[0]][pos[1]] for pos in best_path]
+
+    print(f"Best Reward: {best_reward}")
+    print("Best Path Tokens:", ' '.join(path_tokens))
+
+    # print("Best Path:")
+    # for pos in best_path:
+    #     col_row = (pos[1]+1, pos[0]+1)  # Swap positions to get (col, row)
+    #     print(col_row, end=' ')
+    print(f"Best Path: {best_path}")
+    print(f"Best Trimmed Path: {best_trimmed_path}")
+    print(f"Time taken: {time_taken:.2f} ms")
+
+    # Inside your main function, after computing best_reward, best_path, etc.
+    save_output = input(
+        "\nDo you want to save the results to a file? (yes/no): ")
+    if save_output.lower() == 'yes':
+        save_to_file(best_reward, path_tokens,
+                     best_trimmed_path, time_taken)
+    else:
+        print("Thank you!")
+
+
+if __name__ == "__main__":
+    main()
