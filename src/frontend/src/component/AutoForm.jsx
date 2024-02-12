7a4057d13a9760c4dc6071cabd076b6c5f25faf0
AlbertChoe <albert.choe73@gmail.com>
2024-02-13 01:43:23 +0700

    change auto form

--- src/frontend/src/component/AutoForm.jsx
+++ src/frontend/src/component/AutoForm.jsx
@@ -112,7 +112,7 @@ function AutoForm() {
                         id="maxSequenceSize"
                         value={maxSequenceSize}
                         onChange={(e) => setMaxSequenceSize(e.target.value)}
-                        min="1"
+                        min="2"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-12 text-lg p-2"
                     />
                 </div>
